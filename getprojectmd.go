195c4f6f8db6dcb4db7a1623a67f583abffcc861
Steffen <steffen@fritz.wtf>
2022-06-03 09:42:17 +0000

    working copy

--- getprojectmd.go
+++ getprojectmd.go
@@ -1,1 +1,9 @@
 package gorefine
+
+// ProjectMD is the exported project metadata type
+// returned by /command/core/get-all-project-metadata
+type ProjectMD struct{}
+
+// GetProjectMD is a GET to request all project metadata.
+// This request has no parameters.
+func GetProjectMD() {}
