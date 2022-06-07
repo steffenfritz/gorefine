ff3cd2ff96b8499b76409c0c1f3ab47eac11131b
Steffen Fritz <steffen@fritz.wtf>
2022-06-07 14:45:54 +0200

    added empty service flag check

--- cmd/testclient/main.go
+++ cmd/testclient/main.go
@@ -7,8 +7,14 @@ import (
 )
 
 func main() {
+	// For a full working client we should use a more sophisticated flag package.
+	// However, for this test client we keep external dependencies manageable.
 	serverurl := flag.String("s", "", "The URL of the OpenRefine server, e.g. http://localhost:8080")
 	flag.Parse()
+
+	if len(*serverurl) == 0 {
+		log.Fatalln("-s flag is mandatory. Start this testclient with flag -h for help.")
+	}
 
 	// Create a new http client
 	client := gorefine.NewClient(*serverurl)
